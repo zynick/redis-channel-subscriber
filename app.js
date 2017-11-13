fbdbdb1bfdfb6a11cf04c4adc3596f5d28993146
Zi-Yuan <ziyuan@zynick.com>
2017-11-13 20:12:19 +0800

    add logging

--- app.js
+++ app.js
@@ -15,6 +15,17 @@ const {
 } = require('./config/config');
 
 
+console.log('REDIS_FROM_HOST:', REDIS_FROM_HOST);
+console.log('REDIS_FROM_PORT:', REDIS_FROM_PORT);
+console.log('REDIS_FROM_AUTH:', REDIS_FROM_AUTH);
+console.log('REDIS_FROM_SUBSCRIBE:', REDIS_FROM_SUBSCRIBE);
+console.log('');
+console.log('REDIS_TO_HOST:', REDIS_TO_HOST);
+console.log('REDIS_TO_PORT:', REDIS_TO_PORT);
+console.log('REDIS_TO_AUTH:', REDIS_TO_AUTH);
+console.log('REDIS_TO_QUEUE:', REDIS_TO_QUEUE);
+console.log('');
+
 // connect
 
 const redisFrom = redis.createClient(REDIS_FROM_PORT, REDIS_FROM_HOST);
@@ -31,7 +42,8 @@ if (REDIS_TO_AUTH) {
 // channel from subscribe to queue
 
 redisFrom.on('message', (channel, message) => {
-		redisTo.lpush(REDIS_TO_QUEUE, message);
+    console.log(`    ${message.substr(0,100)}`);
+	redisTo.lpush(REDIS_TO_QUEUE, message);
 });
 
 redisFrom.subscribe(REDIS_FROM_SUBSCRIBE);
